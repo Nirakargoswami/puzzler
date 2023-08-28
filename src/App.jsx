d3a9f151407f24f9e54e0f72a6ce9aa1ab532045
Nirakar <goswaminirakr65@gmail.com>
2023-08-28 23:54:38 +0530

    nnn

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from "react";
 import Home from "./Pages/Home";
-import Quizpage from "./Pages/quizpage"
+import Quizpage from "./Pages/quizpage";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Link,
-  Form,d
+  Form,
+  d,
 } from "react-router-dom";
-import TermsOfService from "../src/Pages/Teremuse"
-import Aboutuse from "../src/Pages/Aboutus"
-import Privasy from "../src/Pages/Privesy"
-import NormalQuiz from "../src/Pages/NoramalQuiz"
-import AdminPanel from "../src/Pages/Adminpannel"
-import Anstpage from "../src/Pages/ansqestionpage"
+import AdsComponent from "./Component/Addsence/Addsence";
+import TermsOfService from "../src/Pages/Teremuse";
+import Aboutuse from "../src/Pages/Aboutus";
+import Privasy from "../src/Pages/Privesy";
+import NormalQuiz from "../src/Pages/NoramalQuiz";
+import AdminPanel from "../src/Pages/Adminpannel";
+import Anstpage from "../src/Pages/ansqestionpage";
 import { useSelector, useDispatch } from "react-redux";
 import ResponsiveDrawer from "../src/Component/Navbar/NAvbar";
 function App() {
@@ -26,10 +28,32 @@ function App() {
   }, [state]);
   const dispatch = useDispatch();
   console.log(state);
+
+  const { dataAdSlot } = props;
+
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {}
+  }, []);
+
   return (
     <>
-      <div style={{marginTop:"80px",padding:"0px 20px 0px 20px",marginBottom:"100px"}}>
-        
+      <div
+        style={{
+          marginTop: "80px",
+          padding: "0px 20px 0px 20px",
+          marginBottom: "100px",
+        }}
+      >
+        <ins
+          className="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-client="ca-pub-XXXXXXXXXXXXXXX"
+          data-ad-slot={dataAdSlot}
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
 
         <ResponsiveDrawer />
         <Routes>
@@ -42,8 +66,6 @@ function App() {
           <Route path="/Legal" element={<Privasy />} />
           <Route path="/About Us" element={<Aboutuse />} />
           <Route path="/Tearms Of Use" element={<TermsOfService />} />
-
-          
         </Routes>
       </div>
     </>
